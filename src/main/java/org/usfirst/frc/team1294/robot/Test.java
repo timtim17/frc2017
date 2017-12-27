c008d29c1edac5f0357d52f0d6655459af743538
Austin Jenchi <ajenchi9@gmail.com>
2017-12-27 15:51:23 -0800

    Create Test.java

---
+++ src/main/java/org/usfirst/frc/team1294/robot/Test.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team1294.robot;
+
+public class Test {
+  public static void main(String [] args){
+  for (int i = 0; i<10;i++ )  
+  {
+    System.out.println (i);
+  }
+  }
+}
+
